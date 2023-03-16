19c1c0289de898a212fdd14c58e3523d2251599c
Tiisetso Dinoko <dinokovonteese@gmail.com>
2023-03-17 01:18:09 +0200

    Committed head

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <div className='text-[#242421] col-span-3 mr-16 w-full my-6 pb-20 h-auto mx-auto flex flex-col  bg-[#f1e4cf] '>
         <div></div>
         <div className='max-w-[800] mt-[40px] container col-span-3  mx-[120px] w-full '>
-            <h1 className='text-4xl font-medium col-span-1 mb-[6px]'>Get your laundry for pickup </h1>
+            <h1 className='text-4xl font-medium col-span-1 mb-[6px]'>Get your laundry for pickup</h1>
             <h1 className='text-4xl  font-medium col-span-1 mb-[20px]'> and delivery today</h1>
             <div className='flex'>
               <p>We'll be right at your door, for your</p>
