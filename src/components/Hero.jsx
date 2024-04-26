f65363febc5f04e380fc14512de13a2e2b26821c
Tiisetso Dinoko <dinokovonteese@gmail.com>
2024-04-26 04:01:48 +0200

    remove the white space beneath nav bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     // bg-[#f1e4cf]
-    <div className='text-[#242421] bg-cover  bg-no-repeat bg-Hero mt-14 md:mt-16'>
+    <div className='text-[#242421] bg-cover  bg-no-repeat bg-Hero'>
        <div className='h-64 md:h-screen'></div>
     {/* Content */}
   </div>
